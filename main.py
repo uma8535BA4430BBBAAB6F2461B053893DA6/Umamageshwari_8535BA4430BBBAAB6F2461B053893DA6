01442b54723d718a811d1082bd1927ed7fecea32
uma8535BA4430BBBAAB6F2461B053893DA6 <144426480+uma8535BA4430BBBAAB6F2461B053893DA6@users.noreply.github.com>
2023-09-21 12:40:48 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-num = int(input("Enter a number: "))
-if num < 0:
-    print("Factorial is not defined for negative numbers.")
-elif num == 0:
-    print("The factorial of 0 is 1")
-else:
-    result = factorial(num)
-    print(f"The factorial of ",num, "is",result)
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Amount must be greater than 0.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
+
+    def display_balance(self):
+        print(f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}")
+
+
+# Create an instance of the BankAccount class
+account1 = BankAccount("123456789", "John Doe", 1000.0)
+
+# Test deposit and withdrawal functionality
+account1.display_balance()
+account1.deposit(500.0)
+account1.withdraw(200.0)
+account1.display_balance()
